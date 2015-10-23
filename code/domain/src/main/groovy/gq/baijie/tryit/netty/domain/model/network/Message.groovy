8ecf87a438fbf86cfd645f600b4564a73a89b971
Bai Jie <baijie1991@gmail.com>
2015-10-23 21:51:14 +0800

    improve Message class

--- code/domain/src/main/groovy/gq/baijie/tryit/netty/domain/model/network/Message.groovy
+++ code/domain/src/main/groovy/gq/baijie/tryit/netty/domain/model/network/Message.groovy
@@ -1,45 +1,28 @@
 package gq.baijie.tryit.netty.domain.model.network
 
-import com.sun.xml.internal.bind.v2.TODO
+import groovy.transform.Immutable
+import okio.ByteString
 
-import java.nio.ByteBuffer
-
+@Immutable(knownImmutableClasses=[ByteString])
 class Message {
     public static final int MAX_FRAME_LENGTH = 2 ** 16 - 1;
     public static final int LENGTH_FIELD_OFFSET = 1;
     public static final int LENGTH_FIELD_LENGTH = 2;
+    public static final int TYPE_FIELD_LENGTH = 1;
+    static {
+        assert Byte.BYTES == TYPE_FIELD_LENGTH
+        assert Short.BYTES == LENGTH_FIELD_LENGTH
+    }
 
     byte type
-    /** the length of {@link #data} */
-    short length
-    ByteBuffer data
+    ByteString data
 
-    static Message fromByteBuffer(ByteBuffer buffer) {
-        Message result = new Message()
-        result.with {
-            type = buffer.get()
-            length = buffer.getShort()
-            data = copyByteBuffer(buffer) //TODO avoid copy
-            assert data.remaining() == length
-        }
-        return result
-    }
-    private static ByteBuffer copyByteBuffer(ByteBuffer buffer) {
-        ByteBuffer result = ByteBuffer.allocate(buffer.remaining())
-        result.put(buffer)
-        result.flip()
-        return result
-    }
-
-    public Message copy() {
-        Message clone = new Message()
-        clone.with {
-            type = this.type
-            length = this.length
-            data = copyByteBuffer(this.data)
-            this.data.rewind()
-        }
-        return clone
+    /**
+     * the length of {@link #data}
+     * @return the length of {@link #data}
+     */
+    int getLength() {
+        data?.size() ?: 0
     }
 
 }
