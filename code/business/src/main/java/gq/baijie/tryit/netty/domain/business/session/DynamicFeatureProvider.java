f25dd09cdf5eefd60525d746bee1ec560a09fde0
Bai Jie <baijie1991@gmail.com>
2015-10-24 11:07:38 +0800

    add DynamicFeatureProvider interface

---
+++ code/business/src/main/java/gq/baijie/tryit/netty/domain/business/session/DynamicFeatureProvider.java
@@ -0,0 +1,24 @@
+package gq.baijie.tryit.netty.domain.business.session;
+
+import gq.baijie.tryit.netty.domain.model.communicate.Message;
+
+public interface DynamicFeatureProvider extends FeatureProvider {
+
+  /**
+   * register a {@link Feature} class whose instance can meet demand(request)
+   * @param requestType what demand(request) this feature can meet
+   * @param featureClass the {@link Feature} class correspond to the {@code requestType}
+   * @see #unregisterFeature
+   * @see Message
+   */
+  void registerFeature(byte requestType, Class<Feature> featureClass);
+
+  /**
+   * unregister a {@link Feature} class registered by {@link #registerFeature}
+   * @param requestType the type of request
+   * @see #registerFeature
+   * @see Message
+   */
+  void unregisterFeature(byte requestType);
+
+}
--- code/business/src/main/java/gq/baijie/tryit/netty/domain/business/session/FeatureProvider.java
+++ code/business/src/main/java/gq/baijie/tryit/netty/domain/business/session/FeatureProvider.java
@@ -1,10 +1,13 @@
 package gq.baijie.tryit.netty.domain.business.session;
 
+import gq.baijie.tryit.netty.domain.model.communicate.Message;
+
 public interface FeatureProvider {
 
     /**
      * get a new {@link Feature} instance who can meet demand(request)
      * @param requestType the type of request
+     * @see Message
      */
     Feature newFeature(byte requestType);
 
