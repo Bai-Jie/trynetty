8d98e756178025f1bb7a55037d6289486b1eb941
Bai Jie <baijie1991@gmail.com>
2015-10-24 11:29:27 +0800

    add FeatureProvider.hasFeature method

--- code/business/src/main/java/gq/baijie/tryit/netty/domain/business/session/FeatureProvider.java
+++ code/business/src/main/java/gq/baijie/tryit/netty/domain/business/session/FeatureProvider.java
@@ -5,6 +5,16 @@ import gq.baijie.tryit.netty.domain.model.communicate.Message;
 public interface FeatureProvider {
 
   /**
+   * has {@link Feature} instance who can meet demand(request)
+   *
+   * @param requestType the type of request
+   * @return true if and only if {@code newFeature(requestType) != null}
+   * @see #newFeature
+   * @see Message
+   */
+  boolean hasFeature(byte requestType);
+
+  /**
    * get a new {@link Feature} instance who can meet demand(request)
    *
    * @param requestType the type of request
